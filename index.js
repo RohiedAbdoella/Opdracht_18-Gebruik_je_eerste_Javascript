6e6f7e2c3e9fc9ef0eb9c009fbb3fc6c10654182
Rohied Abdoella <74419576+RohiedAbdoella@users.noreply.github.com>
2021-01-25 18:31:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+//4
+console.log("Hello Winc Academy")
+
+//5
+let name = "Rohied";
+console.log(name);
+
+//6
+//Je kan "if" niet gebruiken, omdat dit al een 'reserved keyword'is. Een van de regels voor het benoemen van variables. 
+
+//7
+// je kan een som maken met numbers.
+let sumNumber = 2+2;
+console.log(sumNumber);
+
+// je kan geen som maken met strings. hij zet de waarden achter elkaar ipv optellen.
+let sumString = "2"+"2";
+console.log(sumString);
+
+//8
+let addition = 1+1;
+console.log(addition);
+
+let substraction = 2-1;
+console.log(substraction);
+
+let multiplication = 2*5;
+console.log(multiplication);
+
+let division = 5/2;
+console.log(division);
+
+let modulus = 100%2;
+console.log(modulus);
+
+//9
+let age = 39;
+console.log(typeof age);
+
+//10
+let ageNew = "negenendertig";
+console.log(typeof ageNew);
+
